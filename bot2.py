07d087945386d11393c3203a12e69b815fd68119
Steinbloum <alexisperso@as-soudage.com>
2022-06-08 14:24:56 +0200

    avancage fc load

--- bot2.py
+++ bot2.py
@@ -18,7 +18,8 @@ class Botmaker():
     capital : the capital applied to bot'''
     
     
-    def __init__(self, ticker_list, crit_spread, capital) -> None:
+    def __init__(self, ticker_list, crit_spread, capital, load=False) -> None:
+        self.load=load
         self.botlist = False
         self.ticker_list = ticker_list
         self.crit_spread = crit_spread
@@ -53,6 +54,7 @@ class Bot():
         self.wallet = False
         self.transac_hist = False
         self.last_trade = False
+        self.decimals = False
 
     def get_delta (self):
         """Returns a dict {buy, sell}
@@ -88,9 +90,31 @@ class Bot():
                 f'{self.hedge_manager.name}_crypto':crypto_amount, 
                 f'{self.hedge_manager.name}_fiat':fiat_amount}, index=[0])
             df.to_csv(f'csv/wallet_{self.name}.csv', index=False)
+            self.wallet = df
 
-            
-
+    def get_decimals(self, num):
+        string = str(num)
+        dec = len(string.split(".")[1])
+        print(dec)
+        diff = '0.'
+        for n in range(dec):
+            if n == dec-1:
+                diff += "1"
+            else:
+                diff += "0"
+        print(f'diff is {diff}')
+        self.decimals = float(diff)
+    
+    def init_transac_hist(self):
+            if path.exists('csv/trade_hist_{}.csv'.format(self.name)):
+                print(f'{self.name}trade_hist found')
+                t_hist = pd.read_csv('csv/trade_hist_{}.csv'.format(self.name), index_col=False)
+            else :
+                print(f'{self.name}creating trade hist')
+                t_hist = pd.DataFrame(columns=['date', 'exchange', 'side', 'price','qtt', 'value', 'fee'])
+                t_hist.to_csv('csv/trade_hist_{}.csv'.format(self.name), index=False)
+            self.transac_hist = t_hist
+            return t_hist
 
 
 
@@ -102,14 +126,27 @@ for bot in bl :
     pass
 
 class Main_prog():
-    '''for test pupposes'''
+    '''for test pupposes
+    load from file : put path if load'''
     def __init__(self, botlist, load_from_file=False):
-        self.raw_botlist = botlist
-        self.botlist = self.get_botlist()
-        self.ticker_list = self.get_tickerlist()
+        if not load_from_file:
+            self.raw_botlist = botlist
+            self.botlist = self.get_botlist()
+            self.ticker_list = self.get_tickerlist()
+
+        else:
+            self.botlist = []
+            df = pd.read_csv(load_from_file, index_col=False)
+            for n in range(len(df)):
+                self.botlist.append(Bot(df.iloc[n]['ticker'], df.iloc[n]['name'], 1, df.iloc[n]['crit_spread']))
+            self.ticker_list=df['ticker'].drop_duplicates().to_list()
         self.new_trades = {}
         self.obs = {}
-        self.managers = {}       
+        self.managers = {}
+
+    def load_from_file(self):
+        '''reads the bots.csv file and generates the botlist'''
+        pass
 
     def get_tickerlist(self):
         ls = []
@@ -128,6 +165,7 @@ class Main_prog():
 
     def run(self):
 
+        #Start managers
         self.managers['trade']=Krak_man(self.ticker_list)
         self.managers['hedge']=Bin_man(self.ticker_list)
         self.managers['hedge'].ticker_list = self.managers['hedge'].get_ticker(self.ticker_list)
@@ -145,47 +183,38 @@ class Main_prog():
                 pass
                 # print(f'{pair} not in books yet')
         print('all books gathered')
+        #initialise bots
         for bot in self.botlist:
             bot.trade_manager = self.managers['trade']
             bot.hedge_manager = self.managers['hedge']
             bot.init_wallet()
-            # bot.init_transac_hist()
-            print(f'{bot.name} got its mnanagers')
+            bot.get_decimals(bot.trade_manager.books[bot.ticker]['buy'].iloc[0]['price'])
+            bot.init_transac_hist()
+        #store botlist if not present
+        if os.path.isfile('csv/bots.csv'):
+            print('botlist detected')
+        else :
+            df = pd.DataFrame(columns=['name', 'ticker', 'crit_spread', 'decimals'])
+            n=0
+            for bot in self.botlist:
+                df.loc[n] = [bot.name, bot.ticker, bot.crit_spread, bot.decimals]
+                n+=1
+            df.to_csv('csv/bots.csv', index=False)
+                # print(df)
+
+        
+
+# ticker_list = ['XBT/USDT', 'ETH/USDT', 'DOT/USDT', 'LTC/USDT', 'BCH/USDT', 'ADA/USDT', 'EOS/USDT', 'LINK/USDT']
+# bm1 = Botmaker(ticker_list, 0.1, 20000)
+# bl = bm.generate_bot_list()
+# bm2 = Botmaker(ticker_list, 0.5, 10000)
+# bl2 = bm2.generate_bot_list()
 
 
-ticker_list = ['XBT/USDT', 'ETH/USDT', 'DOT/USDT', 'LTC/USDT', 'BCH/USDT', 'ADA/USDT', 'EOS/USDT', 'LINK/USDT']
-bm1 = Botmaker(ticker_list, 0.1, 20000)
-bl = bm.generate_bot_list()
-bm2 = Botmaker(ticker_list, 0.5, 10000)
-bl2 = bm2.generate_bot_list()
-
-
-m = Main_prog([bl, bl2])
-
-
+m = Main_prog(None, load_from_file='csv/bots.csv')
 m.run()
-# print(m.managers['trade'].books)
-# for bot in m.botlist:
-#     m.run()
+print(m.botlist)
+for bot in m.botlist:
+    print(bot.name)
+    print(bot.wallet)
 
-
-
-
-
-
-
-# # demarrer la collecte de data
-# # print(self.managers['hedge'].ticker_list)
-# # input()
-# for ticker in self.managers['hedge'].ticker_list:
-#     tbin = threading.Thread(target=self.managers['hedge'].loop_ob, args = [ticker]) #VOIR POUR FAIR UN FOR LOOP AVEC TICKERLIST
-#     tbin.start()
-# tkrak = threading.Thread(target=self.managers['trade'].gather_data, args = [self.tickerlist])
-# tkrak.start()
-# print('attributin managers to bots')
-# for bot in self.botlist:
-#     bot.trade_manager = self.managers['trade']
-#     bot.hedge_manager = self.managers['hedge']
-#     bot.init_wallet()
-#     bot.init_transac_hist()
-#     print(f'{bot.name} got its mnanagers')
--- tsetalacon.py
+++ tsetalacon.py
@@ -1,6 +1,35 @@
-ls = [['a', 'b'], ['c', 'd']]
+import pandas as pd
 
-if any(isinstance(i, list) for i in ls):
-    print('nest detected')
-    res = [item for sublist in ls for item in sublist]
-    print(res)
+
+class Bao():
+    def __init__(self, load=False) -> None:
+        if load :
+            self.name = 'new'
+        else : self.name = 'false'
+
+b = Bao(load=True)
+print(b.name)
+
+num1 = 1564.25
+num2 = 5.56489874
+
+def count_decimals(num):
+    string = str(num)
+    dec = len(string.split(".")[1])
+    diff = '0.'
+    for n in range(dec):
+        if n == dec-1:
+            diff += "1"
+        else:
+            diff += "0"
+    print(diff)
+    print(float(diff))
+    return diff
+count_decimals(num1)
+count_decimals(num2)
+
+df = pd.read_csv('csv/bots.csv', index_col=False)
+print(df)
+print(len(df))
+tkls = df['ticker'].drop_duplicates().to_list()
+print(tkls)
